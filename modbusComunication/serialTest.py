9c60f2edbfd98f646383ba0efe9d7036d7c1fff8
afbotero89 <afbotero89@gmail.com>
2017-03-23 10:19:55 -0500

    Test para arrancar horno1

--- modbusComunication/serialTest.py
+++ modbusComunication/serialTest.py
@@ -3,23 +3,25 @@
 
 import serial
 
-modbusCommand = '01061192004E2'
+#Comando para arrancar hornos 1
+modbusCommand = '0105080AFF00'
+
+#modbusCommand = '01061192004E2'
 startBit = ':'
 stopbits = '\r\n'
 
 vectorModbus = list(modbusCommand)
 print(vectorModbus)
-checkSum = int('FF',16) - (int(vectorModbus[0] + vectorModbus[1],16) + int(vectorModbus[2] + vectorModbus[3],16) + int(vectorModbus[4] + vectorModbus[5],16) + int(vectorModbus[6] + vectorModbus[7],16) + int(vectorModbus[8] + vectorModbus[9],16) + int(vectorModbus[10] + vectorModbus[11],16)) + 1
+checkSum = int('FF',16) - (int(vectorModbus[0] + vectorModbus[1],16) + int(vectorModbus[2] + vectorModbus[3],16) + int(vectorModbus[4] + vectorModbus[5],16) + int(vectorModbus[6] + vectorModbus[7],16) + int(vectorModbus[8] + vectorModbus[9],16) + int(vectorModbus[10] + vectorModbus[11],16)) + 1 + 256
 #print('FF',vectorModbus[0] + vectorModbus[1], vectorModbus[2] + vectorModbus[3], vectorModbus[4] + vectorModbus[5], vectorModbus[6] + vectorModbus[7], vectorModbus[8] + vectorModbus[9], vectorModbus[10] + vectorModbus[11])
 print(checkSum)
-print(hex(checkSum))
 checkSum = hex(checkSum).split('x')[1]
-#print(checkSum)
+print(checkSum.upper())
 
 #completeModbusCommand = startBit + modbusCommand + checkSum + stopbits
 #print(completeModbusCommand)
 
-s = serial.Serial('/dev/ttyUSB0',9600)
+s = serial.Serial('/dev/tty.SLAB_USBtoUART',9600)
 s.bytesize = 7
 s.parity = serial.PARITY_EVEN
 s.stopbits = 1
@@ -27,7 +29,7 @@ s.stopbits = 1
 #s.write(bytes(':010611C2004E2' + 'FF' + '\r\n','UTF-8'))
 #tiempoMuestreo = s.read(11)
 #print(tiempoMuestreo)
-comando = bytes(':0106119A004FF2\r\n','UTF-8')
+comando = bytes(':0105080AFF00E9\r\n','UTF-8')
 s.write(comando)
 lectura = s.readline()
 print(lectura, comando)
